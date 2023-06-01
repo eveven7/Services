4cda4a50842187d51f3751331c20de788e3584ba
Evelina <evelina.venckute@swedbank.lt>
2023-06-01 16:48:26 +0300

    using services

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
-import { LoggingService } from "./logging.service";
-@Injectable()// receiving service to be injected
+import { EventEmitter, Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
+@Injectable() // receiving service to be injected
 export class AccountsService {
   accounts = [
     {
@@ -16,15 +16,15 @@ export class AccountsService {
       status: 'unknown',
     },
   ];
-  constructor(private loggingService:LoggingService){}
+
+  statusUpdated = new EventEmitter<string>();
+  constructor(private loggingService: LoggingService) {}
   addAccount(name: string, status: string) {
     this.accounts.push({ name: name, status: status });
-    this.loggingService.logStatusChange(status)
+    this.loggingService.logStatusChange(status);
   }
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
-    this.loggingService.logStatusChange(status)
-
+    this.loggingService.logStatusChange(status);
   }
-
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,7 +12,9 @@ export class NewAccountComponent {
   constructor(
     private loggingService: LoggingService,
     private accountsService: AccountsService
-  ) {}
+  ) {
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('new status '+ status));
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
