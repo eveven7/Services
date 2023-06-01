99bace658a9854e464e46cb8a77736ef1eaf8b15
Evelina <evelina.venckute@swedbank.lt>
2023-06-01 16:38:51 +0300

    injecting services

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+@Injectable()// receiving service to be injected
 export class AccountsService {
   accounts = [
     {
@@ -13,10 +16,15 @@ export class AccountsService {
       status: 'unknown',
     },
   ];
+  constructor(private loggingService:LoggingService){}
   addAccount(name: string, status: string) {
     this.accounts.push({ name: name, status: status });
+    this.loggingService.logStatusChange(status)
   }
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status)
+
   }
+
 }
--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,8 +1,6 @@
-
-
-export class LoggingService{
-logStatusChange(status:string){
-  console.log('A server status changed, new status: ' + status);
-
+export class LoggingService {
+  logStatusChange(status: string) {
+    console.log('A server status changed, new status: ' + status);
+  }
 }
-}
+//we do not inject anything in this service
